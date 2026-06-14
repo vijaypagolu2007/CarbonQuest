016367017b3e4138b26959e58d2f840b5bce3a14
vijaypagolu2007 <pagoluvijay7@gmail.com>
2026-06-14 10:19:04 +0530

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+    ],
+  },
+  allowedDevOrigins: ['192.168.1.2'],
+}
+
+module.exports = nextConfig
